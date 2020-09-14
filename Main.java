3c830faa888afca3cd32bc1804f45baef76c0b24
zesha5816 <zesha5816@wrdsb.ca>
2020-09-14 13:15:18 +0000

    Finished the example question

--- Main.java
+++ Main.java
@@ -1,15 +1,41 @@
+
+import java.util.Scanner;
 /**
- *
- * @author 
- */
+* A program to say hello to a user and calculate their approximate age
+* @author Ahmad Zeshan
+*/
 public class Main {
 
-  /**
-   * The method that is executed when you hit the run button.
-   * @param args the command line arguments
-   */
-  public static void main(String[] args) {
-    // TODO code application logic here
-    
-  }
+ /**
+  * The method that is executed when you hit the run button.
+  * @param args the command line arguments
+  */
+ public static void main(String[] args) {
+	 //create a scanner for user input 
+	 Scanner input = new Scanner(System.in);
+
+   // declare a variable that will store a name
+   String userName;
+
+   // ask the user for their name
+   System.out.println("Please enter your name:");
+   // initialize the name variable with Bob
+   userName = input.nextLine();
+   // Say hello to the user
+   System.out.println("Hello " + userName + "!");
+  
+   // declare and initialize a constant to store the current year
+   int CURRENT_YEAR = 2020;
+   // Ask the user for the year they were born
+   System.out.println("Please enter the year you were born:");
+   // declare and initialize a variable to store the year they were born
+   int birthYear = input.nextInt();
+
+   // declare and calculate their age
+   int age = CURRENT_YEAR - birthYear;
+   // Tell the user their age
+   System.out.println("You are " + age + " years old.");
+
+ }
 }
+
